7d410602c9616bcf5663e77624c35ba2027f2587
lobaojoao <joao.lobao@southsystem.com.br>
2024-01-18 09:35:36 -0300

    test: added mock in application

--- src/tests/__mocks__/firebase.js
+++ src/tests/__mocks__/firebase.js
@@ -1,25 +1,16 @@
-const mockSignIn = jest.fn(() => Promise.resolve({ user: 'mockUser' }));
-const mockSignOut = jest.fn(() => Promise.resolve());
-const mockSendPasswordResetEmail = jest.fn((email) => Promise.resolve());
+export const mockSignIn = jest.fn(() => Promise.resolve({ user: 'mockUser' }));
+export const mockSignOut = jest.fn(() => Promise.resolve());
+export const sendPasswordResetEmail = jest.fn((email) => Promise.resolve());
 
-const mockOnAuthStateChanged = jest.fn((callback) => {
+export const mockOnAuthStateChanged = jest.fn((callback) => {
   const user = { email: 'test@example.com', uid: '12345' };
   callback(user);
   return jest.fn();
 });
 
-const simulateUserLogout = () => {
+export const simulateUserLogout = () => {
   mockOnAuthStateChanged.mockImplementation((callback) => {
     callback(null);
     return jest.fn();
   });
-};
-
-export default {
-  simulateUserLogout,
-  mockSendPasswordResetEmail,
-  signInWithEmailAndPassword: mockSignIn,
-  signOut: mockSignOut,
-  onAuthStateChanged: mockOnAuthStateChanged,
-  sendPasswordResetEmail: mockSendPasswordResetEmail,
 };
