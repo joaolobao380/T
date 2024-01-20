eb91b09cb9ef31bf36e98d9d350b5961b87baccd
lobaojoao <joao.lobao@southsystem.com.br>
2024-01-20 18:37:53 -0300

    feat: added types to use svg

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,19 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-// eslint-disable-next-line no-undef
-const config = getDefaultConfig(__dirname);
-config.resolver.sourceExts.push('cjs');
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...resolver.sourceExts, 'svg'],
+  };
+
+  return config;
+})();
