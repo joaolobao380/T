5a50cd840b955a111e2afa3685f6bd7a8e7848e0
lobaojoao <joao.lobao@southsystem.com.br>
2024-01-20 18:43:31 -0300

    feat: added screen forgot password on navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,5 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StackNavigationProp, createStackNavigator } from '@react-navigation/stack';
+import { ForgotPassword } from '@screens/forgotPassword';
+import { Login } from '@screens/login';
 
 import TabNavigator from './tab-navigator';
 import Modal from '../screens/modal';
@@ -7,24 +9,32 @@ import Modal from '../screens/modal';
 export type RootStackParamList = {
   TabNavigator: undefined;
   Modal: undefined;
+  Login: undefined;
+  ForgotPassword: undefined;
 };
+
+export type NavigatatorRoutesProps = StackNavigationProp<RootStackParamList>;
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function RootStack() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="TabNavigator">
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="ForgotPassword"
+          component={ForgotPassword}
+          options={{
+            headerShown: false,
+          }}
+        />
         <Stack.Screen
           name="TabNavigator"
           component={TabNavigator}
           options={{ headerShown: false }}
         />
-        <Stack.Screen
-          name="Modal"
-          component={Modal}
-          options={{ presentation: 'modal', headerLeft: null }}
-        />
+        <Stack.Screen name="Modal" component={Modal} options={{ presentation: 'modal' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
