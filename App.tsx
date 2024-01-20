83de7cd69a5ccc03ed0fbb55847e4f4e0c80678e
lobaojoao <joao.lobao@southsystem.com.br>
2024-01-20 18:37:10 -0300

    feat: created splash screen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,44 @@
 import 'react-native-gesture-handler';
 
+import { toastConfig } from '@helpers/configToast';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import { View, StyleSheet } from 'react-native';
 import Toast from 'react-native-toast-message';
 
 import RootStack from './src/navigation';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-Light': require('./assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-SemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <RootStack />
-      <Toast />
-    </>
+      <Toast config={toastConfig} />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
