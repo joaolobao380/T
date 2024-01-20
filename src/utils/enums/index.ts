b78f8f185f1912206ef7dbc3cbbe3e574a40dde8
lobaojoao <joao.lobao@southsystem.com.br>
2024-01-20 18:40:55 -0300

    feat: created enum

---
+++ src/utils/enums/index.ts
@@ -0,0 +1,20 @@
+export enum messageToast {
+  ERROR = 'customError',
+  SUCCESSFUL = 'successful',
+}
+
+export enum errorMessageFirebase {
+  TOO_MANY = 'auth/too-many-requests',
+  INVALID_PASSWORD = 'auth/invalid-password',
+  INVALID_EMAIL = 'auth/invalid-email',
+  INVALID_CREDENTIAL = 'auth/invalid-credential',
+}
+
+export enum textToast {
+  ATTENTION = 'Atenção',
+  SUCCESSFUL = 'Sucesso!',
+}
+
+export enum positionToast {
+  POSITION = 'bottom',
+}
