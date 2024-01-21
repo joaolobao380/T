2b76213a52fda797645c95816ea57c9f74596555
lobaojoao <joao.lobao@southsystem.com.br>
2024-01-20 22:21:44 -0300

    chore: added temporary logout

--- src/screens/one.tsx
+++ src/screens/one.tsx
@@ -1,12 +1,19 @@
+import { Button } from '@components/Button';
+import { useAuth } from '@hooks/useAuth';
 import { StyleSheet, Text, View } from 'react-native';
 
 import EditScreenInfo from '../components/edit-screen-info';
 
 export default function TabOneScreen() {
+  const { logout } = useAuth();
+  const handleLogout = async () => {
+    await logout();
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} />
+      <Button onPress={() => handleLogout()} title="Sair" isLoading={false} />
       <EditScreenInfo path="src/screens/one.tsx" />
     </View>
   );
